f5af85dd07b1b1f2489057cbf0a8ea425f2e0f4a
Arka Banerjee <arkabanerjee381@gmail.com>
2025-10-13 13:14:36 +0530

    python

---
+++ WEEK-1/typecasting.py
@@ -0,0 +1,14 @@
+# Typecasting: to change a datatype from one to another
+
+a = int (10.1) # this will convert 10.1 to 10
+
+print (a) # prints 10
+
+b = float (10) # this will convert 10 to 10.0
+
+print (b)
+
+c = str(10.3) # this will convert 10.3 to '10.3'
+
+print (c)
+
