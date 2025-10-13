5392f060e9c5affe8c1de84102e9a721e6892033
Arka Banerjee <arkabanerjee381@gmail.com>
2025-10-13 13:56:36 +0530

    python

--- WEEK-1/8-logical-operators.py
+++ WEEK-1/8-logical-operators.py
@@ -1,1 +1,12 @@
+# Logical operators 
 
+"""
+
+and -> returns True if both the LHS and RHS are true
+
+or -> returns False if both the LHS and RHS are false
+
+not -> reverses the boolean value 
+        example : not (True) returns False
+
+"""
