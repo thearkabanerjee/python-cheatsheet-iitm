025d43323a09362a4cb1e499ed6c2b7845d09ad5
Arka Banerjee <arkabanerjee381@gmail.com>
2025-10-13 13:45:24 +0530

    python

---
+++ WEEK-1/7-relational-operators.py
@@ -0,0 +1,22 @@
+# Relational Operators
+
+# it will compare and give True or False
+
+"""
+
+> -> Greater than
+
+< -> Less than 
+
+>= -> Greater than or equal to
+
+<= -> Less than or equal to
+
+== -> Equal to
+
+!= -> Not Equal
+
+
+
+
+"""
