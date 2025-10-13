c53f2109254b2e94f03fa997c7a3c246e83ffbb0
Arka Banerjee <arkabanerjee381@gmail.com>
2025-10-13 13:36:01 +0530

    python

---
+++ WEEK-1/arithmetic-operators.py
@@ -0,0 +1,18 @@
+# arithmetic operators
+
+""" 
++ -> Addition (int or float) , Concatenation (str)
+
+- -> Substraction
+
+* -> multiplication (int or float), Repetation (str)
+
+/ -> Division
+
+// -> Floor Division (Integer Division)
+
+% -> Modulus Division (remainder after division)
+
+** -> Exponentiation (power)
+
+"""
