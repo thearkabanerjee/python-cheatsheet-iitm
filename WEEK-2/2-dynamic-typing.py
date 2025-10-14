bcea1f1703ed3eee6226e71188570a5c29f5d524
Arka Banerjee <arkabanerjee381@gmail.com>
2025-10-14 10:48:17 +0530

    python

---
+++ WEEK-2/2-dynamic-typing.py
@@ -0,0 +1,16 @@
+#dynamic typing:
+
+# the type of a variable is determined by the 
+
+# In Python, dynamic typing means that you don’t have to
+# declare the type of a variable when you create it — the
+# interpreter figures it out at runtime based on the value you assign.
+
+''' in short;
+* variable types are determined at runtime
+
+* you can reassign variables to different types freely 
+
+
+
+'''
