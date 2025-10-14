3afbd560ce190f29f60e2336b85dc1f63a15218a
Arka Banerjee <arkabanerjee381@gmail.com>
2025-10-14 11:12:59 +0530

    python

--- WEEK-2/4-deleting-variables.py
+++ WEEK-2/4-deleting-variables.py
@@ -7,5 +7,6 @@ print ("the value of x is:", x) # the value of x is 12
 
 del x # deletes the variable
 
-print (x) # prints out a ERROR cz there's no x variable anymore
+# prints out a ERROR cz there's no x variable anymore
+print (x) 
 
