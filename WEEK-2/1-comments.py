5abb8a94a8276377afc96bc19f893fbde1134a57
Arka Banerjee <arkabanerjee381@gmail.com>
2025-10-14 10:41:11 +0530

    python

--- WEEK-2/1-comments.py
+++ WEEK-2/1-comments.py
@@ -3,3 +3,8 @@
 # a section of the code that gets ignored by the compiler
 # (it is added to increase the readability & understanding of the code)
 
+# this is a comment
+
+''' this is 
+a mutili-line 
+comment. '''
