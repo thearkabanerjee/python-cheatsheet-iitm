6e82bbf5cb17096b534d3b9296a7c78b426e2f5e
Arka Banerjee <arkabanerjee381@gmail.com>
2025-10-16 13:05:49 +0530

    python

--- WEEK-2/8-escape-characters.py
+++ WEEK-2/8-escape-characters.py
@@ -3,3 +3,10 @@
 #To insert characters that are illegal in a string, use an escape character.
 
 #An escape character is a backslash \ followed by the character you want to insert.
+
+'''
+
+print(‘It’s Raining’) -> Error #All Characters after second ‘ (single quote) are not considered as a part of the string (s Raining)
+
+'''
+
