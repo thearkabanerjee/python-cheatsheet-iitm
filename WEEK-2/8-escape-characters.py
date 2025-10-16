c7bf9d97338d61b0a287e07c92ceea69160933b9
Arka Banerjee <arkabanerjee381@gmail.com>
2025-10-16 13:04:44 +0530

    python

--- WEEK-2/8-escape-characters.py
+++ WEEK-2/8-escape-characters.py
@@ -1,3 +1,5 @@
 # escape characters
 
 #To insert characters that are illegal in a string, use an escape character.
+
+#An escape character is a backslash \ followed by the character you want to insert.
